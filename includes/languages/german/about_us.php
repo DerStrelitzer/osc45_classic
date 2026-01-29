f1b21a774d9bd1308fe4eb2c125a413fa50e32c8
DerStrelitzer <ingo@strelitzer.de>
2026-01-29 22:08:28 +0100

    Update about_us.php

--- includes/languages/german/about_us.php
+++ includes/languages/german/about_us.php
@@ -1,1 +1,20 @@
+<?php
+/*
+  $Id: about_us.php,v 1.00 2005/01/01 Ingo <www.strelitzer.de>
 
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2026 xPrioS
+  Copyright (c) 2004 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Wir über uns');
+define('HEADING_TITLE', 'Wir über uns');
+define('TEXT_INFORMATION', '
+
+Hier werden Sie in Kürze einiges Wissenswertes über uns erfahren können.
+
+');
